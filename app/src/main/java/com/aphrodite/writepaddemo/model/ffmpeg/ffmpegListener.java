d271b61dfc503091da34e95f4dfd36943a9ebc8f
Aphrodite123 <927467936@qq.com>
2021-04-01 10:10:09 +0800

    Delete useless file.

--- app/src/main/java/com/aphrodite/writepaddemo/model/ffmpeg/ffmpegListener.java
+++
@@ -1,7 +0,0 @@
-package com.aphrodite.writepaddemo.model.ffmpeg;
-
-public interface ffmpegListener {
-    void onBegin();
-
-    void onEnd(int resultCode, String resultMsg);
-}
--- app/src/main/java/com/aphrodite/writepaddemo/view/inter/IBaseView.java
+++
@@ -1,7 +0,0 @@
-package com.aphrodite.writepaddemo.view.inter;
-
-/**
- * Created by Aphrodite on 2018/8/2.
- */
-public interface IBaseView {
-}
